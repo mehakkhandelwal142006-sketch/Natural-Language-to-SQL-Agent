29ddd671148d458067a0d2f924bcfc350c42375b
mehak khandelwal <mehakkhandelwal142006@gmail.com>
2026-08-06 09:31:20 +0530

    Update database.py

--- database.py
+++ database.py
@@ -197,16 +197,3 @@ def validate_sql(sql):
 
     return any(sql.startswith(keyword) for keyword in valid_keywords)
 
-def get_tables(db_path=None):
-    """
-    Returns list of tables from the selected database.
-    """
-
-    engine = get_engine(db_path)
-
-    with engine.connect() as conn:
-        result = conn.execute(
-            text("SELECT name FROM sqlite_master WHERE type='table';")
-        )
-
-        return [row[0] for row in result]
